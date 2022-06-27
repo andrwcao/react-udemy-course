79257f7411cc7fdd575c63b5ec5376b79d4b8b07
andrwcao <80874259+andrwcao@users.noreply.github.com>
2022-06-27 21:12:10 +1000

    Section 13 completed

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -3,6 +3,9 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from "./components/Route";
+import Header from "./components/Header";
+
 const items = [
     {
         title: 'What is React?',
@@ -17,7 +20,7 @@ const items = [
         content: 'You use React by creating components'
     },
 ];
-/*
+
 const options = [
     {
         label: 'The Color Red',
@@ -32,30 +35,32 @@ const options = [
         value: 'blue'
     }
 ]
-*/
 
-export default () => {
+const App = () => {
+    const [selected, setSelected] = useState(options[0]);
+
     return (
-        /*
         <div>
-            <Accordion items={items}/>
-        </div>
-
-        <div>
-        <Search />
-        </div>
-        */
-        /*
-        <div>
-        <Dropdown 
-            selected={selected} 
-            setSelected={setSelected} 
-            options={options} 
-        />
-        </div>
-        */
-        <div>
-        <Translate />
+            <Header />
+            <Route path="/">
+            <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+            <Search />
+            </Route>
+            <Route path="/dropdown">
+            <Dropdown
+                label="Select a color"
+                options={options}
+                selected={selected}
+                onSelectedChange={setSelected}
+            />
+            </Route>
+            <Route path="/translate">
+            <Translate />
+            </Route>
         </div>
     );
 };
+
+export default App;
