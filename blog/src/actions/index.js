3efcba04404987dc7e7ae06fed6b24c66b22facb
andrwcao <80874259+andrwcao@users.noreply.github.com>
2022-07-05 01:18:26 +1000

    Section 19 completed

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,4 +1,12 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+  
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+  };
 
 export const fetchPosts = () => {
     return async function (dispatch, getState) {
@@ -6,8 +14,35 @@ export const fetchPosts = () => {
 
         dispatch({
             type: 'FETCH_POSTS',
-            payload: response
+            payload: response.data
         });
     }
 };
 
+export const fetchUser = (id) => {
+    return async function (dispatch, getState) {
+        const response = await jsonPlaceholder.get(`/users/${id}`);
+
+        dispatch({
+            type: 'FETCH_USER',
+            payload: response.data
+        });
+    }
+}
+
+/*
+export const fetchUser = (id) => {
+    return function (dispatch, getState) {
+        _fetchUser(id, dispatch);
+    };
+}
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    });
+});
+*/
