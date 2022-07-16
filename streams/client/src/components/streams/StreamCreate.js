3898938f2d809cefce8442877a571fc0d76cdc6a
andrwcao <80874259+andrwcao@users.noreply.github.com>
2022-07-16 13:43:47 +1000

    Section 23 completed

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,9 +1,63 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-    return (
-        <div>A</div>
-    );
+class StreamCreate extends React.Component {
+    renderInput = (formProps) => {
+        const className = `field ${formProps.meta.error && formProps.meta.touched ? 'error' : ''}`;
+        return (
+            <div className={className}>
+                <label>{formProps.label}</label>
+                <input {...formProps.input} />
+                {this.renderError(formProps.meta)}
+            </div>
+        );
+    }
+
+    renderError(meta) {
+        const { touched, error } = meta;
+        if (touched && error) {
+            return (
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            );
+        }
+    }
+
+    onSubmit(formValues) {
+        console.log('efjkwnfow');
+    }
+
+    render() {
+        return (
+            <form 
+                onSubmit={this.props.handleSubmit(this.onSubmit)} 
+                className="ui form error"
+            >
+                <Field name="title" component={this.renderInput} label="Enter title" />
+                <Field name="description" component={this.renderInput} label="Enter description" />
+                <button className="ui button primary">Submit</button>
+            </form>
+        );
+    };
 }
 
-export default StreamCreate;
+const validate = (formValues) => {
+    const errors = {};
+
+    if (!formValues.title) {
+        errors.title = 'You must enter a title';
+    }
+
+    if (!formValues.description) {
+        errors.description = 'You must enter a description';
+    }
+
+    return errors;
+
+}
+
+export default reduxForm({
+    form: 'streamCreate',
+    validate
+})(StreamCreate);
--- streams/client/src/reducers/authReducer.js
+++ streams/client/src/reducers/authReducer.js
@@ -6,9 +6,9 @@ const INITIAL_STATE = {
 };
 
 export default (state = INITIAL_STATE, action) => {
-    if (action.type == SIGN_IN) {
+    if (action.type === SIGN_IN) {
         return { ...state, isSignedIn: true, userId: action.payload };
-    } else if (action.type == SIGN_OUT) {
+    } else if (action.type === SIGN_OUT) {
         return { ...state, isSignedIn: false, userId: null };
     }
 
